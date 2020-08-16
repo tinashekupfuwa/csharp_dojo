8c6d98ef332fa6271047cce5beec421f5be64c4d
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-08-16 07:39:14 +0200

    c# loops

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace csharp_practice_02
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace csharp_practice_02 {
+    class Program {
+        static void Main (string[] args) {
+
+            string guess;
+            int noOfguesses = 10;
+            int tries = 0;
+            string keyword = "sadza";
+            // bool validTry = true;
+
+            Console.WriteLine ("What is the staple food of Zimbabwe");
+            guess = Console.ReadLine ();
+
         }
     }
 }
