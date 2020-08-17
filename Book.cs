f50662c7b99cc0b247dce2426a6702414483c5a2
tinashekupfuwa <tinashekupfuwa@gmail.com>
2020-08-17 08:36:18 +0200

    c# added-class-book

---
+++ Book.cs
@@ -0,0 +1,19 @@
+namespace csharp_practice_02
+{
+  public class Book
+  {
+    public string title;
+    public string author;
+    public int pages;
+
+    public Book(string aTitle, string aAuthor, int aPages)
+    {
+      title = aTitle;
+      author = aAuthor;
+      pages = aPages;
+
+    }
+
+
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 
-namespace csharp_practice_02 {
-    class Program {
-        static void Main (string[] args) {
+namespace csharp_practice_02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
 
-            string guess;
-            int noOfguesses = 10;
-            int tries = 0;
-            string keyword = "sadza";
-            // bool validTry = true;
+      // // string guess;
+      // // int noOfguesses = 10;
+      // // int tries = 0;
+      // // string keyword = "sadza";
+      // // bool validTry = true;
 
-            Console.WriteLine ("What is the staple food of Zimbabwe");
-            guess = Console.ReadLine ();
+      // // Console.WriteLine("What is the staple food of Zimbabwe");
+      // // guess = Console.ReadLine();
 
-        }
+      // int[,] numberGrid = {
+      //   {1,2},
+      //   {3,4},
+      //   {5,6}
+      // };
+
+      // Console.WriteLine(numberGrid[2, 1]);
+
+      // int[,] myArray = new int[2, 3]; // This is just awa y of declaring arrays when you have no data to fill them.
+
+      // //Exception handling
+
+      Book book1 = new Book("1234", "2345", 200);
+      Console.WriteLine(book1.title);
+
     }
+  }
 }
